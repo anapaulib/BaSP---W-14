475182ead436c7bbf62ef7645d8477db379900ff
anapaulib <anapaulib@gmail.com>
2022-11-22 13:37:48 -0300

    BaSP-W-14: negative tests

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,41 +1,28 @@
-
-
-import Page from './page';
-
-/**
- * sub page containing specific selectors and methods for a specific page
- */
-class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+class LoginPage {
     get inputUsername () {
-        return $('#username');
+        return $('#user-name');
     }
 
     get inputPassword () {
         return $('#password');
     }
 
-    get btnSubmit () {
-        return $('button[type="submit"]');
+    get loginButton () {
+        return $('#login-button');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    get menuButton () {
+        return $('#react-burger-menu-btn');
+    }
+
+    get errorMessage () {
+        return $('#login_button_container > div > form > div.error-message-container.error > h3');
+    }
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
+        await this.loginButton.click();
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,32 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
 
-describe('My Login application', () => {
+describe('Login', () => {
+    beforeAll ('Navigate to url', () => {
+        browser.url ('https://www.saucedemo.com/');
+    });
+
+    it ('should not login with empty inputs', async () => {
+        await LoginPage.login('', '');
+        await expect (LoginPage.errorMessage).toHaveText('Epic sadface: Username is required');
+    })
+
+    it ('should not login without user', async () => {
+        await LoginPage.login('', 'secret_sauce');
+        await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Username is required');
+    })
+
+    it ('should not login without password', async () => {
+        await LoginPage.login('standard_user', '');
+        await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Password is required');
+    })
+
+    it ('should not login with non valid credentials', async () => {
+        await LoginPage.login('Alguien', 'Contraseña');
+        await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    })
+
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await expect(LoginPage.menuButton).toBeExisting();
     });
 });
-
-
